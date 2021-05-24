86ee7444b9a2bc2f36a60cc9a6d443cecd5b9ba7
shiraazAh <shiraaz.ahammed@gmail.com>
2021-05-24 19:10:04 +0530

    Calculate component movement

--- src/components/expenses/ExpenseMotionComponent.js
+++ src/components/expenses/ExpenseMotionComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ExpensesContainer from "./ExpensesContainer";
 import { motion } from 'framer-motion'
 import useWindowDimensions from '../extras/useWindowDimensions'
@@ -8,6 +8,8 @@ const ExpenseMotionContainer = () => {
   const [animate, setAnimate] = useState('startAnimate')
 //   const [scrollPos, setScrollPos] = useState(1)
   const { height, width } = useWindowDimensions();
+  let compHeight = height;
+  const compMovePercentage = useRef(100) //The percentage value of how much percentage of the screen size did the component bee dragged 
 
   // useEffect(() => { // Initial Animation is true (should happen), then after 1.5 seconds it should be removed (should'nt happen)
   //   setTimeout(() => {
@@ -44,12 +46,17 @@ const ExpenseMotionContainer = () => {
         transition={{duration: 0.5, delay: 1.5}}
           onDrag={
             (event, info) => {
-              console.log(info.point.x, info.point.y, info, height, width)
-              if(info.point.y <= 700 && info.point.y >= 400) {
-                // setScrollPos((info.point.y - 400)/300)
-              }
+              let move = compHeight;
+              console.log(move, event.movementY, height, width)
+              compHeight = move + event.movementY;
+              compMovePercentage.current = (((compHeight)/height) * 100)
+              console.log(compHeight, compMovePercentage)
             }
-          }/>
+          }
+          // DirectionLock={axis => {
+          //   console.log(axis);
+          // }}
+          />
         {/* <motion.div style={{background: 'white', height: '40px',}}drag></motion.div>   */}
     </div>
   );
