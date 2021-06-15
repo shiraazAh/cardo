22de99acbbd929d4108debda2d7171da9e8bfcc6
shiraazAh <shiraaz.ahammed@gmail.com>
2021-06-15 22:35:52 +0530

    Add a mobile layout and a new component for desktop screens

---
+++ src/screen/DesktopVersion.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import AllCards from '../components/cards/AllCards';
+import ExpenseMotionContainer from '../components/expenses/ExpenseMotionComponent'
+// import useWindowDimensions from '../components/extras/useWindowDimensions'
+
+import '../App.css';
+
+function App() {
+//   const { height, width } = useWindowDimensions();
+  const [cardColor, setCardColor] = useState('brown');
+
+  return (
+    <div className="App-desktop example" style={{background: 'black'}}>
+        <AllCards setCardColor={setCardColor}/>
+        <ExpenseMotionContainer selectedCard={cardColor}/>
+    </div>
+  );
+}
+
+export default App;
