132c1b7c296ed121838fea2c3aa9374d77c145c2
shiraazAh <shiraaz.ahammed@gmail.com>
2021-06-19 23:44:52 +0530

    Change card details to unique ones

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -20,18 +20,18 @@ const creditCard = React.forwardRef((props, ref) => {
         <div className={props.startAnimation ? `${styles.cardBlue} ${styles.animationBlue}` : styles.cardBlue}>
             <img className={styles.logoBrown} src={logoBlue} alt="master card"></img>
             <p className={`${styles.cardHolder} ${styles.textWhite}`}>SHIRAAZ &nbsp; AHAMMED</p>
-            <p className={`${styles.cardAmount} ${styles.textWhite}`}>$5,750,20</p>
-            <p className={`${styles.cardNumber} ${styles.textWhite}`}>5282 3456 7890 1289</p>
-            <p className={`${styles.cardDate} ${styles.textWhite}`}>9/25</p>
+            <p className={`${styles.cardAmount} ${styles.textWhite}`}>$2,750,20</p>
+            <p className={`${styles.cardNumber} ${styles.textWhite}`}>4163 1863 6740 4520</p>
+            <p className={`${styles.cardDate} ${styles.textWhite}`}>1/22</p>
         </div>
     </div> : 
     props.violet ? <div ref={ref}>
         <div className={props.startAnimation ? `${styles.cardViolet} ${styles.animationViolet}` : styles.cardViolet}>
             <img className={styles.logoBrown} src={logoViolet} alt="master card"></img>
             <p className={`${styles.cardHolder} ${styles.textWhite}`}>SHIRAAZ &nbsp; AHAMMED</p>
-            <p className={`${styles.cardAmount} ${styles.textWhite}`}>$5,750,20</p>
-            <p className={`${styles.cardNumber} ${styles.textWhite}`}>5282 3456 7890 1289</p>
-            <p className={`${styles.cardDate} ${styles.textWhite}`}>9/25</p>
+            <p className={`${styles.cardAmount} ${styles.textWhite}`}>$7,750,20</p>
+            <p className={`${styles.cardNumber} ${styles.textWhite}`}>6532 1579 3456 5356</p>
+            <p className={`${styles.cardDate} ${styles.textWhite}`}>13/27</p>
         </div>
     </div> : null
 )})
