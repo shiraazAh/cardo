792670e2f1f9455990aa173cb70a6c1e55488c33
shiraazAh <shiraaz.ahammed@gmail.com>
2021-06-20 17:23:05 +0530

    New Addition: Described in description
    1) Added new input to type in user name; 2) Added exit animations; 3) Add context api to pass user name;

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,16 +1,19 @@
-import React, {useState} from "react"
+import React, {useContext} from "react"
+import NameContext from "../../context/context"
 import styles from "./cards.module.css"
 import logoBrown from "../../assets/cardLogos/logoforbrown.png"
 import logoBlue from "../../assets/cardLogos/logoforblue.png"
 import logoViolet from "../../assets/cardLogos/logoforviolet.png"
 
 const creditCard = React.forwardRef((props, ref) => {
+
+    const userName = useContext(NameContext)
     
     return(
     props.brown ? <div ref={ref}>
         <div className={props.startAnimation ? `${styles.cardBrown} ${styles.animationBrown}` : styles.cardBrown}>
             <img className={styles.logoBrown} src={logoBrown} alt="master card"></img>
-            <p className={styles.cardHolder}>SHIRAAZ &nbsp; AHAMMED</p>
+            <p className={styles.cardHolder}>{userName}</p>
             <p className={styles.cardAmount}>$5,750,20</p>
             <p className={styles.cardNumber}>5282 3456 7890 1289</p>
             <p className={styles.cardDate}>9/25</p>
@@ -19,7 +22,7 @@ const creditCard = React.forwardRef((props, ref) => {
     props.blue ? <div ref={ref}>
         <div className={props.startAnimation ? `${styles.cardBlue} ${styles.animationBlue}` : styles.cardBlue}>
             <img className={styles.logoBrown} src={logoBlue} alt="master card"></img>
-            <p className={`${styles.cardHolder} ${styles.textWhite}`}>SHIRAAZ &nbsp; AHAMMED</p>
+            <p className={`${styles.cardHolder} ${styles.textWhite}`}>{userName}</p>
             <p className={`${styles.cardAmount} ${styles.textWhite}`}>$2,750,20</p>
             <p className={`${styles.cardNumber} ${styles.textWhite}`}>4163 1863 6740 4520</p>
             <p className={`${styles.cardDate} ${styles.textWhite}`}>1/22</p>
@@ -28,7 +31,7 @@ const creditCard = React.forwardRef((props, ref) => {
     props.violet ? <div ref={ref}>
         <div className={props.startAnimation ? `${styles.cardViolet} ${styles.animationViolet}` : styles.cardViolet}>
             <img className={styles.logoBrown} src={logoViolet} alt="master card"></img>
-            <p className={`${styles.cardHolder} ${styles.textWhite}`}>SHIRAAZ &nbsp; AHAMMED</p>
+            <p className={`${styles.cardHolder} ${styles.textWhite}`}>{userName}</p>
             <p className={`${styles.cardAmount} ${styles.textWhite}`}>$7,750,20</p>
             <p className={`${styles.cardNumber} ${styles.textWhite}`}>6532 1579 3456 5356</p>
             <p className={`${styles.cardDate} ${styles.textWhite}`}>13/27</p>
--- src/screen/startScreen.js
+++ src/screen/startScreen.js
@@ -1,5 +1,11 @@
 import React, { useRef, useEffect, useState } from "react";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import NameContext from "../context/context";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import DesktopVersion from "./DesktopVersion";
 import IphoneXLayout from "../assets/iPhone X - Silver.png";
 import "../App.css";
@@ -7,6 +13,8 @@ import "../App.css";
 const StartScreen = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const [themeSelected, setThemeSelected] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [nameGiven, setNameGiven] = useState("");
 
   const x = useMotionValue(80);
   const xInput = [-80, 0, 80];
@@ -54,57 +62,101 @@ const StartScreen = () => {
     }
   };
 
+  const handleInput = (e) => {
+    let value = e.target.value;
+    setUserName(value);
+  };
+
+  const handleName = () => {
+    setNameGiven(true);
+  };
+
   return (
-    <motion.div
-      className="startScreen-container"
-      style={{ background }}
-    >
-    {!themeSelected ? (
-      <>
-        <motion.div className="startScreen-header" style={{ color }}>
-          Select a theme
-        </motion.div>
-        <motion.p
-          className="startScreen-light"
-          style={{ color }}
-          animate={isDarkTheme === true ? "notSelected" : "selected"}
-          variants={textVariants}
-        >
-          Light
-        </motion.p>
-        <motion.div className="drag-area" ref={constraintsRef} />
-        <motion.div
-          className="box"
-          style={{ x }}
-          animate={isDarkTheme === true ? "dark" : "light"}
-          variants={boxVariants}
-          whileTap={{ scale: 1.1 }}
-          onTapStart={() => onChangeTheme()}
-        />
-        <motion.p
-          className="startScreen-dark"
-          style={{ color }}
-          animate={isDarkTheme === true ? "selected" : "notSelected"}
-          variants={textVariants}
-        >
-          Dark
-        </motion.p>
-        <motion.button
-          className="startScreen-button"
-          whileHover={{ scale: 1.1, boxShadow: "1px 1px 10px #aaa" }}
-          onClick={() => setThemeSelected(true)}
-          style={{ border, color }}
-        >
-          Select
-        </motion.button>
-      </>
-      ) : (
-        <motion.div className="App example" style={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.5, delay: 0.3}}>
-          <img className="iPhoneX-layout" src={IphoneXLayout}></img>
-          <DesktopVersion />
-        </motion.div>
-      )}
-    </motion.div>
+    <NameContext.Provider value={userName}>
+      <motion.div className="startScreen-container" style={{ background }}>
+        <AnimatePresence>
+          {!themeSelected && !nameGiven && (
+            <motion.div key="select-theme" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+              <motion.div 
+                className="startScreen-header" 
+                style={{ color }}>
+                  Select a theme
+              </motion.div>
+              <motion.p
+                className="startScreen-light"
+                style={{ color }}
+                animate={isDarkTheme === true ? "notSelected" : "selected"}
+                variants={textVariants}
+              >
+                Light
+              </motion.p>
+              <motion.div className="drag-area" ref={constraintsRef} />
+              <motion.div
+                className="box"
+                style={{ x }}
+                animate={isDarkTheme === true ? "dark" : "light"}
+                variants={boxVariants}
+                whileTap={{ scale: 1.1 }}
+                onTapStart={() => onChangeTheme()}
+              />
+              <motion.p
+                className="startScreen-dark"
+                style={{ color }}
+                animate={isDarkTheme === true ? "selected" : "notSelected"}
+                variants={textVariants}
+              >
+                Dark
+              </motion.p>
+              <motion.button
+                className="startScreen-button"
+                onClick={() => setThemeSelected(true)}
+                style={{ border, color }}
+              >
+                Next
+              </motion.button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {themeSelected && !nameGiven && (
+              <motion.div key="select-theme" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5}} exit={{opacity: 0}}>
+              <motion.div 
+              className="startScreen-name-header" style={{ color }}>
+                Enter the name you want shown on the cards
+              </motion.div>
+              <motion.input
+                className={`startScreen-input ${
+                  isDarkTheme ? "input-dark" : "input-light"
+                }`}
+                onChange={handleInput}
+                type="text"
+                style={{ border, color }}
+              ></motion.input>
+              <motion.button
+                className="startScreen-name-button"
+                onClick={() => handleName()}
+                style={{ border, color }}
+              >
+                Start
+              </motion.button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {themeSelected && nameGiven && (
+            <motion.div
+              className="App example"
+              style={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+            >
+              <img className="iPhoneX-layout" src={IphoneXLayout}></img>
+              <DesktopVersion userName={userName} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </NameContext.Provider>
   );
 };
 
